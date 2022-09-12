436a89cace595d45286b221cc7f4b94832f26a60
Hridoy Banik <hridoy.banik71@gmail.com>
2022-09-12 23:06:05 +0600

    add primay crud

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,25 @@
-const express = require('express')
-const cors = require('cors')
-const planetRouter = require('./routes/planets/planets.router')
+const path = require('path');
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
 
-const app = express()
+const planetsRouter = require('./routes/planets/planets.router');
+const launchesRouter = require('./routes/launches/launches.router');
+
+const app = express();
 
 app.use(cors({
-    origin:'http://localhost:3000'
-}))
-app.use(express.json())
-app.use(planetRouter)
+  origin: 'http://localhost:3000',
+}));
+app.use(morgan('combined'));
 
-module.exports = app
+app.use(express.json());
+//app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.use('/planets', planetsRouter);
+app.use('/launches', launchesRouter);
+// app.get('/*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+// });
+
+module.exports = app;
--- server/src/routes/planets/planets.router.js
+++ server/src/routes/planets/planets.router.js
@@ -2,6 +2,6 @@ const express = require('express')
 
 const planetRouter = express.Router()
 const {getAllPlanets} = require('./planets.controller')
-planetRouter.get('/planets', getAllPlanets)
+planetRouter.get('/', getAllPlanets)
 
 module.exports = planetRouter
